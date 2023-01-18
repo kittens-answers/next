81915ec485030e2e027f324aff63e1b55709eb8d
Rychanya <rychanya@gmail.com>
2023-01-18 12:24:24 +0300

    🎉 init api

---
+++ api/api/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_item():
+    return {"method": "get"}
+
+
+@app.post("/")
+def read_item_():
+    return {"method": "post"}
