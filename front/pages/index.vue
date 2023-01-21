b33d2a285f76c134fcff12cacdc673a479174269
Rychanya <rychanya@gmail.com>
2023-01-21 15:12:29 +0300

    🚀

---
+++ front/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        Test api {{ fetchData }}
+        <button v-on:click="fetchApi">click</button>
+        <p>{{ tg }}</p>
+    </div>
+</template>
+
+<script setup>
+const fetchData = ref("")
+const tg = useTG()
+const fetchApi = async () => {
+    const { data: payload } = await useFetch('https://stage-api.kittensanswers.ru/', { method: 'POST' })
+    fetchData.value = payload.value
+}
+</script>
