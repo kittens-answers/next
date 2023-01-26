20715fd40f62ef642fd0a818696505d211166eda
Rychanya <rychanya@gmail.com>
2023-01-26 09:54:47 +0300

    🚀

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -6,7 +6,9 @@ export default defineNuxtConfig({
                 { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" }
             ],
             script: [
-                { src: "https://telegram.org/js/telegram-web-app.js" }
+                { src: "https://telegram.org/js/telegram-web-app.js" },
+                { type: "module", src: "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js", tagPosition: "bodyClose" },
+                { nomodule: true, src: "https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js", tagPosition: "bodyClose" }
             ]
         }
     }
--- front/pages/questions/[aid].vue
+++ front/pages/questions/[aid].vue
@@ -1,7 +1,34 @@
 <template>
-    <div>
-        {{ route.params.aid }}
-    </div>
+    <section class="section">
+        <div class="container">
+            <form class="box">
+                <div class="field has-addons">
+                    <div class="control is-expanded">
+                        <input name="q" class="input" type="text" placeholder="Поиск в котиках">
+                    </div>
+                    <div class="control">
+                        <button class="button is-info">
+                            <span class="icon">
+                                <ion-icon name="search-outline"></ion-icon>
+                            </span>
+                        </button>
+                    </div>
+                </div>
+                <div class="field">
+                    <div class="control">
+                        <label class="checkbox">
+                            <input type="checkbox" name="only-correct" checked value="True">
+                            Только верные ответы
+                        </label>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+    <section class="section">
+        <div class="container">
+        </div>
+    </section>
 </template>
 
 <script setup>
